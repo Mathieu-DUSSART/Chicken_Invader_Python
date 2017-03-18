6a87c540089ca6255b92d9cec6569f096260dd61
Mathieu-DUSSART <mathieud17700@gmail.com>
2017-03-18 14:53:20 +0100

    Création du projet

---
+++ Base_multi/base_client_m.py
@@ -0,0 +1,209 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+"""
+    Very simple game with Pygame
+"""
+import sys, pygame
+import os
+import time
+from pygame.locals import *
+import random
+from PodSixNet.Connection import connection, ConnectionListener
+
+# FUNCTIONS
+def load_png(name):
+    """Load image and return image object"""
+    fullname=os.path.join('.',name)
+    try:
+        image=pygame.image.load(fullname)
+        if image.get_alpha is None:
+            image=image.convert()
+        else:
+            image=image.convert_alpha()
+    except pygame.error:
+        print('Cannot load image: %s' % name)
+        raise SystemExit
+    return image,image.get_rect()
+
+
+# PODSIXNET
+class GameClient(ConnectionListener):
+
+    def __init__(self, host, port):
+        self.Connect((host, port))
+        self.run = False
+
+	### Network event/message callbacks ###
+    def Network_connected(self, data):
+        print('client connecte au serveur !')
+
+    def Network_start(self, data):
+        self.run = True
+        screen.blit(background_image, background_rect)
+
+    def Network_error(self, data):
+        print('error: %s', data['error'][1])
+        connection.Close()
+
+    def Network_disconnected(self, data):
+        print('Server disconnected')
+        sys.exit()
+
+
+# CLASSES
+class Vaisseau(pygame.sprite.Sprite, ConnectionListener):
+    """Class for the player"""
+
+    def __init__(self, number):
+        pygame.sprite.Sprite.__init__(self)
+        self.shot_group = pygame.sprite.RenderClear()
+
+	self.number = number
+        if number == 1:
+            self.image,self.rect=load_png("Pics/vaisseau_r.png")
+            self.image_n,_ = load_png("Pics/vaisseau_r.png")
+            self.rect.center = [SCREEN_WIDTH/2 - 100, SCREEN_HEIGHT/2]
+        elif number == 2:
+            self.image,self.rect=load_png("Pics/vaisseau_b.png")
+            self.image_n,_ = load_png("Pics/vaisseau_b.png")
+            self.rect.center = [SCREEN_WIDTH/2 + 100, SCREEN_HEIGHT/2]
+
+    def Network_vaisseau(self,data):
+        self.image = self.image_n
+        self.rect.center = data['vaisseau' + str(self.number)][0:2]
+
+    #def Network_shots(self,data):
+        #self.shot_group.add(Shot(data['shots'][0:2], 'n'))
+
+    def update(self):
+        self.Pump()
+
+class Chicken(pygame.sprite.Sprite):
+    """Class for the player"""
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        #self.shot_group = pygame.sprite.RenderClear()
+
+        self.image,self.rect=load_png("Pics/chicken.png")
+        self.rect.center = [100, 100]
+
+    def Network_chicken(self,data):
+        self.rect.center = data['chicken'][0:2]
+
+    def update(self, position):
+        self.rect.center = position[0:2]
+
+class Shot(pygame.sprite.Sprite):
+    '''
+    The player's shots
+    '''
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_png('Pics/shot.png')
+
+    def update(self, position):
+        self.rect.center = position
+
+
+class SpriteGroup(pygame.sprite.RenderClear, ConnectionListener):
+    '''
+    Sprite groups (shots, mechs)
+    '''
+    def __init__(self, action):
+        pygame.sprite.RenderClear.__init__(self)
+        self.action = action
+
+    def Network(self, data):
+        if data['action'] == self.action:
+            elems = data[self.action]
+            num_sprites = len(self.sprites())
+
+            if num_sprites < len(elems): # we're missing sprites
+                for additional_sprite in range(len(elems) - num_sprites):
+                    self.add_elem()
+            elif num_sprites > len(elems): # we've got too many
+                deleted = 0
+                to_delete = num_sprites - len(elems)
+                for sprite in self.sprites():
+                    self.remove(sprite)
+                    deleted += 1
+                    if deleted == to_delete:
+                        break
+
+            for indice, sprite in enumerate(self.sprites()):
+                sprite.update(elems[indice])
+
+    def add_elem(self):
+        if self.action == 'shots':
+            self.add(Shot())
+        elif self.action == 'chickens':
+            self.add(Chicken())
+
+    def update(self):
+        self.Pump()
+
+
+# MAIN
+if __name__ == '__main__':
+    SCREEN_WIDTH = 800
+    SCREEN_HEIGHT = 600
+
+    # PodSixNet init
+    game_client = GameClient(sys.argv[1],int(sys.argv[2]))
+
+    # Init Pygame
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    pygame.key.set_repeat(1,1)
+
+    # Elements
+    background_image, background_rect = load_png('Pics/background.jpg')
+    wait_image, wait_rect = load_png('Pics/wait1.png')
+    wait_rect.center = [ SCREEN_WIDTH/2, SCREEN_HEIGHT/2 ]
+    screen.blit(background_image, background_rect)
+    vaisseau_sprite = pygame.sprite.RenderClear()
+    #chicken_sprite = pygame.sprite.RenderClear()
+    #chicken_sprite.add(Chicken())
+    vaisseau_sprite.add(Vaisseau(1))
+    vaisseau_sprite.add(Vaisseau(2))
+    shot_group = SpriteGroup('shots')
+    chicken_group = SpriteGroup('chickens')
+
+    while True:
+        clock.tick(60)
+        connection.Pump()
+        game_client.Pump()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit(0)
+
+        if game_client.run:
+            keys = pygame.key.get_pressed()
+            if keys[K_q]:
+                sys.exit(0)
+            connection.Send({'action':'keys','keystrokes':keys})
+
+            # updates
+            vaisseau_sprite.update()
+            #chicken_sprite.update()
+            shot_group.update()
+            chicken_group.update()
+
+            # drawings
+            vaisseau_sprite.clear(screen, background_image)
+            vaisseau_sprite.draw(screen)
+            #chicken_sprite.clear(screen, background_image)
+            #chicken_sprite.draw(screen)
+            shot_group.clear(screen, background_image)
+            shot_group.draw(screen)
+            chicken_group.clear(screen, background_image)
+            chicken_group.draw(screen)
+
+        else: # game is not running
+            screen.blit(wait_image, wait_rect)
+
+        pygame.display.flip()
---
+++ Base_multi/base_server_m.py
@@ -0,0 +1,253 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+"""
+    Very simple game with Pygame
+"""
+
+import sys, pygame
+import os
+import time
+from pygame.locals import *
+import random
+from PodSixNet.Channel import Channel
+from PodSixNet.Server import Server
+import time,sys
+
+
+
+# FUNCTIONS *******************
+def load_png(name):
+    """Load image and return image object"""
+    fullname=os.path.join('.',name)
+    try:
+        image=pygame.image.load(fullname)
+        if image.get_alpha is None:
+            image=image.convert()
+        else:
+            image=image.convert_alpha()
+    except pygame.error:
+        print('Cannot load image: %s' % name)
+        raise SystemExit
+    return image,image.get_rect()
+
+# GAME CLASSES ****************
+class Vaisseau(pygame.sprite.Sprite):
+    """Class for the player"""
+
+    def __init__(self, number):
+        pygame.sprite.Sprite.__init__(self)
+        self.shot_group = pygame.sprite.RenderClear()
+        self.image, self.rect = load_png('Pics/soldier_n.png')
+        if number == 1:
+            self.rect.center = [SCREEN_WIDTH/2 - 100, SCREEN_HEIGHT/2]
+        elif number == 2:
+            self.rect.center = [SCREEN_WIDTH/2 + 100, SCREEN_HEIGHT/2]
+
+    def update(self, keys):
+        if keys[K_UP]:
+            if self.rect.center[1] >= 0:
+                self.rect = self.rect.move([0,-10])
+        elif keys[K_DOWN]:
+            if self.rect.center[1] <= SCREEN_HEIGHT:
+                self.rect = self.rect.move([0,10])
+        elif keys[K_LEFT]:
+            if self.rect.center[0] >= 0:
+                self.rect = self.rect.move([-10,0])
+        elif keys[K_RIGHT]:
+            if self.rect.center[0] <= SCREEN_WIDTH:
+                self.rect = self.rect.move([10,0])
+
+class Chicken(pygame.sprite.Sprite):
+    """Class for the player"""
+    coord_x = 50
+    coord_y = 50
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        #self.shot_group = pygame.sprite.RenderClear()
+
+        self.image,self.rect=load_png("Pics/chicken.png")
+        #coord_x = random.randrange(100, SCREEN_WIDTH, 100)
+        #coord_y = random.randrange(100, 250, 72)
+        self.rect.center = [Chicken.coord_x, Chicken.coord_y]
+        Chicken.coord_x += 100
+        if(Chicken.coord_x >= SCREEN_WIDTH):
+            Chicken.coord_x = 50
+            Chicken.coord_y += 72
+
+
+    def update(self):
+        #self.Pump()
+        ''' '''
+
+class Shot(pygame.sprite.Sprite):
+    '''
+    The player's shots
+    '''
+    def __init__(self,center,orientation):
+        pygame.sprite.Sprite.__init__(self)
+        self.image,self.rect=load_png("Pics/shot.png")
+        self.rect.center = center
+        speeds = {'nw':[-1,-1], 'ne':[1,-1], 'n':[0,-1], 'sw':[-1,1], 'se':[1,1], 's':[0,1], 'w':[-1,0], 'e':[1,0]}
+        self.vector = [15 * x for x in speeds[orientation]]
+
+    def update(self):
+        self.rect=self.rect.move(self.vector)
+        if((self.rect.top>SCREEN_WIDTH) or (self.rect.bottom<0)):
+            self.kill()
+        if((self.rect.left>SCREEN_WIDTH) or (self.rect.right<0)):
+            self.kill()
+
+
+# PODSIXNET *********************
+class ClientChannel(Channel):
+
+    def __init__(self, *args, **kwargs):
+        Channel.__init__(self, *args, **kwargs)
+        self.shot_group = pygame.sprite.RenderClear()
+        self.shooting = 0
+
+    def create_vaisseau(self, number):
+        self.vaisseau = Vaisseau(number)
+
+    def create_chicken(self):
+        self.chicken = Chicken()
+
+    def Close(self):
+        self._server.del_client(self)
+
+    def Network_keys(self,data):
+        keys = data['keystrokes']
+        if self.shooting != 0:
+            self.shooting -= 1
+        if keys[K_SPACE]:
+            if self.shooting == 0:
+                self.shot_group.add(Shot(self.vaisseau.rect.center, 'n'))
+                self.shooting = 12
+        self.vaisseau.update(keys)
+
+    def update(self, chicken_group):
+        self.shot_group.update()
+        #self.send_shots()
+
+        pygame.sprite.groupcollide(chicken_group, self.shot_group, True, True, pygame.sprite.collide_circle_ratio(0.7))
+        #collision = pygame.sprite.spritecollide(self.vaisseau, chicken_group, False, pygame.sprite.collide_circle_ratio(0.5))
+
+        self.send_chickens(chicken_group)
+
+
+    def send_chickens(self, chicken_group):
+        #chicken1 = self.clients[0].chicken
+        #message1 = [ chicken1.rect.centerx, chicken1.rect.centery ]
+        chickens = []
+        for chicken in chicken_group.sprites():
+            chickens.append([chicken.rect.centerx, chicken.rect.centery])
+
+
+        self.Send({"action":'chickens', 'chickens': chickens})
+
+
+        #for client in self.clients:
+        #    client.Send({'action':'chicken', 'vaisseau':message1})
+
+
+# SERVER
+class MyServer(Server):
+
+    channelClass = ClientChannel
+
+    def __init__(self, *args, **kwargs):
+        Server.__init__(self, *args, **kwargs)
+        self.clients = []
+        self.run = False
+        pygame.init()
+        self.shot_group = pygame.sprite.RenderClear()
+        self.screen = pygame.display.set_mode((128, 128))
+        print('Server launched')
+
+    def Connected(self, channel, addr):
+        self.clients.append(channel)
+        channel.create_vaisseau(len(self.clients))
+        channel.create_chicken()
+        print('New connection: %d client(s) connected' % len(self.clients))
+
+        if len(self.clients) == 2:
+            for client in self.clients:
+                client.Send({'action':'start'})
+            self.run = True
+            wheels_image, wheels_rect = load_png('Pics/wheels.png')
+            self.screen.blit(wheels_image, wheels_rect)
+
+    def del_client(self,channel):
+        print('client deconnecte')
+        self.clients.remove(channel)
+
+    # SENDING FUNCTIONS
+    def send_vaisseaux(self):
+        vaisseau1 = self.clients[0].vaisseau
+        self.shot1 = self.clients[0].shot_group
+        message1 = [ vaisseau1.rect.centerx, vaisseau1.rect.centery ]
+        vaisseau2 = self.clients[1].vaisseau
+        self.shot2 = self.clients[1].shot_group
+        message2 = [ vaisseau2.rect.centerx, vaisseau2.rect.centery ]
+
+        for client in self.clients:
+            client.Send({'action':'vaisseau', 'vaisseau1':message1, 'vaisseau2':message2})
+
+
+
+    def send_shots(self):
+        shots = []
+        for shot in self.shot1:
+            shots.append(shot.rect.center)
+        for shot in self.shot2:
+            shots.append(shot.rect.center)
+        for client in self.clients:
+            client.Send({"action":'shots', 'shots': shots})
+
+    def update_channels(self, chicken_group):
+        for client in self.clients:
+            client.update(chicken_group)
+
+    # MAIN LOOP
+    def launch_game(self):
+        """Main function of the game"""
+        test = 0
+
+        # Init Pygame
+        pygame.display.set_caption('Server')
+        clock = pygame.time.Clock()
+        pygame.key.set_repeat(1,1)
+
+        # Elements
+        wait_image, wait_rect = load_png('Pics/wait.png')
+        self.screen.blit(wait_image, wait_rect)
+        self.chicken_group = pygame.sprite.RenderClear()
+
+        while True:
+            clock.tick(60)
+            self.Pump()
+
+            if self.run:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        return
+
+                # updates
+                self.send_vaisseaux()
+                self.update_channels(self.chicken_group)
+                if test < 25:
+                    self.chicken_group.add(Chicken())
+                    test = test + 1
+                self.send_shots()
+
+            pygame.display.flip()
+
+
+# PROGRAMME INIT
+if __name__ == '__main__':
+    SCREEN_WIDTH = 800
+    SCREEN_HEIGHT = 600
+    my_server = MyServer(localaddr = (sys.argv[1],int(sys.argv[2])))
+    my_server.launch_game()
